b7066ba9d8f4e00ecc116ef0ebf09ad06bfae163
Brandon Fancher <brandonfancher@gmail.com>
2017-08-07 09:10:02 -0400

    Add backup phrase to preferences.

--- src/components/PreferencesDrawer.js
+++ src/components/PreferencesDrawer.js
@@ -9,9 +9,17 @@ const PreferencesDrawer = ({ closeDrawer }) => (
   <View style={styles.container}>
     <StaticNavBar label="Preferences" onBack={closeDrawer} />
     <ScrollView style={styles.scrollView}>
+
       <H2>Backup Phrase</H2>
-      <View style={styles.bodyGroup}>
-        
+      <View style={[styles.bodyGroup, styles.centerContents]}>
+        <Text style={styles.backupPhrase}>
+          {process.env.TEST_MNEMONIC ? process.env.MNEMONIC : ''}
+        </Text>
+      </View>
+
+      <H2>Development</H2>
+      <View style={[styles.bodyGroup, styles.centerContents]}>
+
       </View>
     </ScrollView>
   </View>
@@ -29,9 +37,19 @@ const styles = StyleSheet.create({
   bodyGroup: {
     backgroundColor: 'white',
     height: 80,
+    paddingHorizontal: 20,
+  },
+  centerContents: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   scrollView: {
     backgroundColor: CONSTANTS.COLORS.background,
+  },
+  backupPhrase: {
+    color: CONSTANTS.COLORS.blackText,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
